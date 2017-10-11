167d7b7ebc002371e0d225b159a6bf08a4a7b62a
Administrator <Administrator@MA1IL6ON5L9S6E6>
2017-10-12 07:49:32 +0800

    qq

---
+++ src/main/java/com/czy/spring/source/SpringTest.java
@@ -0,0 +1,20 @@
+package com.czy.spring.source;
+
+import org.springframework.beans.factory.BeanFactory;
+import org.springframework.beans.factory.xml.XmlBeanFactory;
+import org.springframework.core.io.ClassPathResource;
+
+/** 
+* @author: 
+* @version:  
+* @since：2017年10月11日 下午11:50:55
+* 类说明:
+*/
+@SuppressWarnings("deprecation")
+public class SpringTest {
+	public static void main(String[] args) {
+		BeanFactory bf = new XmlBeanFactory(new ClassPathResource("spring-servlet.xml"));
+		Test test = bf.getBean(Test.class);
+		System.out.println(test.getTest());
+	}
+}
---
+++ src/main/java/com/czy/spring/source/Test.java
@@ -0,0 +1,21 @@
+package com.czy.spring.source;
+
+/** 
+* @author: 
+* @version:  
+* @since：2017年10月11日 下午11:50:05
+* 类说明:
+*/
+public class Test {
+	private String test = "ttttt";
+
+	public String getTest() {
+		return test;
+	}
+
+	public void setTest(String test) {
+		this.test = test;
+	}
+	
+	
+}
